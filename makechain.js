be2760685561a5d2380eb5d7a5b98ef21282d238
Muhammad Talha Zulfiqar <muhammadtalha4163@gmail.com>
2023-09-13 19:17:28 +0500

    Updates

--- makechain.js
+++ makechain.js
@@ -8,13 +8,6 @@ import { loadQAStuffChain } from "langchain/chains";
 
 export const makeChain = () => {
 
-
-    // const questionPromptTemplateString = `Context information is below.
-    // ---------------------
-    // {context}
-    // ---------------------
-    // Given the context information , answer the question: {question}`;
-
     const questionPromptTemplateString = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
     If you don't know the answer, just say you don't know in French Language . DO NOT try to make up an answer.
     If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
@@ -30,7 +23,7 @@ export const makeChain = () => {
         inputVariables: ["context", "question"],
         template: questionPromptTemplateString,
     });
-    
+
     const refinePromptTemplateString = `
     The original question is as follows: {question}
     We have provided an existing answer: {existing_answer}
